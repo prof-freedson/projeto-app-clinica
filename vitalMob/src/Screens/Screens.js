3b39eee44f2e6db80c7d7e1e32a31b618c96b914
DanielMaster23~ <danielmendesstudy@gmail.com>
2023-08-16 23:22:45 -0300

    Vital Mob Isadm

--- vitalMob/src/Screens/Screens.js
+++
@@ -1,3 +0,0 @@
-export { default as LoginScreen } from '../Components/Login'
-export { default as PainelScreen } from '../admin/Home/Painel'
-export { default as RegistrationScreen } from '../Components/Registre'
