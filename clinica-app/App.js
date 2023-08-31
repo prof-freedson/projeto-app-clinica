4f06314c3ab2e6f9a29ccd0dc628b744111198a1
aluno-vanderson <vanderson.belfort2019@gmail.com>
2023-08-31 15:33:58 -0300

    Modificado arquivo Agendar Exames

--- clinica-app/App.js
+++ clinica-app/App.js
@@ -2,16 +2,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
 import Exames from './screens/Exames';
-import testeData from './screens/TesteData';
+import TesteData from './screens/TesteData';
+import AgendarExames from './screens/AgendarExames';
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="testeData" screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName="agendarExame" screenOptions={{headerShown: false}}>
         <Stack.Screen name='exames' component={Exames} />
-        <Stack.Screen name='testeData' component={testeData} />
+        <Stack.Screen name='testeData' component={TesteData} />
+        <Stack.Screen name='agendarExame' component={AgendarExames}/>
       </Stack.Navigator>
     </NavigationContainer >
   );
--- clinica-app/screens/Exames.js
+++ clinica-app/screens/Exames.js
@@ -79,6 +79,9 @@ export default function Exames({ navigation }) {
                     imagem={require('../assets/images/endoscopia.jpg')}
                 />
             </ScrollView >
+            {/* <Pressable styele={styles.botaoExame} onPress={() => navigation.navigate('agendarexame')}>
+                <Text>AGENDAR NOVO EXAME</Text>
+            </Pressable> */}
         </View >
     )
 }
@@ -112,5 +115,15 @@ const styles = StyleSheet.create({
         fontSize: 15,
         color: '#ffffff'
 
+    },
+    botaoExame: {
+        width: '100%',
+        height: 35,
+        backgroundColor: '#eacb57',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+        marginTop: 10,
+        borderRadius: 5
     }
 })
