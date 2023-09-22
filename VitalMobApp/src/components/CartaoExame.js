a9c947bde397e9b9affaad0fab383ee1a1744bb1
aluno-vanderson <vanderson.belfort2019@gmail.com>
2023-09-22 17:40:11 -0300

    Alteração no arquivo Cartão Exame de Components

--- VitalMobApp/src/components/CartaoExame.js
+++ VitalMobApp/src/components/CartaoExame.js
@@ -1,19 +1,27 @@
 import { Alert, Text, Image, View, Pressable, StyleSheet } from "react-native";
 
-export default function CartaoExame({nome, data, hora, preco, orientaTitulo, orientaDetalhes, imagem}){
-    return(
+export default function CartaoExame({ nome, data, hora, preco, orientaTitulo, orientaDetalhes, imagem }) {
+    return (
         <View style={styles.exameImagem}>
 
-                <Image style={{ width: '30%', height: 120, objectFit: 'cover', borderTopLeftRadius: 10, borderBottomLeftRadius: 10 }} source={imagem} />
-                <View style={{ backgroundColor: '#ffffff', height: 120, width: '70%', fontSize: 20, fontWeight: 'bold', paddingLeft: 10, justifyContent: 'center', borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>
-                    <Text style={styles.cartaoTexto}>{nome}</Text>
-                    <Text style={styles.cartaoTexto}>Data: {data}  </Text>
-                    <Text style={styles.cartaoTexto}>Horário: {hora}</Text>
-                    <Text style={styles.cartaoTexto}>Preço: R$ {preco}</Text>
-                    <Pressable style={styles.botaoAlerta} onPress={() => Alert.alert(orientaTitulo, orientaDetalhes)}>
-                        <Text style={styles.botaoTexto}>Orientações: Clique aqui!</Text>
-                    </Pressable>
-                </View>
+            <TouchableOpacity>
+                <ImageBackground style={{ width: 111, height: 120 }} source={imagem} imageStyle={{ borderTopLeftRadius: 10, borderBottomLeftRadius: 10 }}>
+                    
+                    <Text style={{ alignItems: 'center', justifyContent: 'center', color: '#fff', backgroundColor: '#dd242cc0', fontSize: 10, fontWeight: 'bold', margin: 10, borderRadius: 5, lineHeight: 10, textAlign: 'center' }}>Clique na imagem {'\n'} para obter mais{'\n'} informações!</Text>
+                </ImageBackground>
+            </TouchableOpacity>
+
+            <View style={{ backgroundColor: '#ffffff', height: 120, width: '70%', fontSize: 20, fontWeight: 'bold', paddingLeft: 10, justifyContent: 'center', borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>
+
+                <Text style={styles.cartaoTexto}>{nome}</Text>
+                <Text style={styles.cartaoTexto}>Data: {data}  </Text>
+                <Text style={styles.cartaoTexto}>Horário: {hora}</Text>
+                <Text style={styles.cartaoTexto}>Preço: R$ {preco}</Text>
+
+                <Pressable style={styles.botaoAlerta} onPress={() => Alert.alert(orientaTitulo, orientaDetalhes)}>
+                    <Text style={styles.botaoTexto}>Orientações: Clique aqui!</Text>
+                </Pressable>
+            </View>
         </View>
     )
 }
@@ -35,10 +43,7 @@ const styles = StyleSheet.create({
         paddingLeft: 15
     },
     botaoAlerta: {
-        borderTopLeftRadius: 5,
-        borderTopRightRadius: 5,
-        borderBottomLeftRadius: 5,
-        borderBottomRightRadius: 5,
+        borderRadius: 5,
         backgroundColor: '#dd242c'
     },
     botaoTexto: {
