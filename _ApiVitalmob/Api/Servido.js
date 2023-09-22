ba993331edb3dc4e61da8de9d60ebedf7038e27e
DanielMaster23~ <danielmendesstudy@gmail.com>
2023-09-22 12:32:46 -0300

    Atualização API Servidor Obetendo e Modificando os Dados ✔

--- _ApiVitalmob/Api/Servido.js
+++ _ApiVitalmob/Api/Servido.js
@@ -16,23 +16,205 @@ const db = admin.firestore();
 const userCollection = db.collection('usuarios');
 
 
-// userCollection.get()
-// .then(dados =>{
-//   dados.forEach(doc => {
-//     const dados = doc.data();
-//     console.log(dados)
+const filiaisCollection = db.collection('filiais');
+
+// filiaisCollection.get()
+//   .then(dados => {
+//     dados.forEach(doc => {
+//       const dadosFiliais = doc.data();
+//       console.log("Dados da coleção 'filiais':", dadosFiliais);
+
+//       // Agora, você pode acessar subcoleções se elas estiverem presentes nos dados do documento.
+//       // Vamos supor que há uma subcoleção chamada "funcionarios" em cada documento.
+
+//       const mapaCollection = doc.ref.collection('mapa');
+
+//       mapaCollection.get()
+//         .then(mapaDados => {
+//           mapaDados.forEach(mapaDoc => {
+//             const dadosMapa = mapaDoc.data();
+//             console.log("Dados da subcoleção 'Mapa':", dadosMapa);
+//           });
+//         })
+
+//       const informacoes_contato = doc.ref.collection('informacoes_contato');
+
+//       informacoes_contato.get()
+//         .then(contatosDados => {
+//           contatosDados.forEach(contatoDoc => {
+//             const dadoscontatos = contatoDoc.data();
+//             console.log("Dados da subcoleção 'Mapa':", dadoscontatos);
+//           });
+//         })
+
+//       const imagens = doc.ref.collection('imagens');
+//       imagens.get()
+//         .then(imgDados => {
+//           imgDados.forEach(imgDoc => {
+//             const dadosimg = imgDoc.data();
+//             console.log("Dados da subcoleção 'Mapa':", dadosimg);
+//           });
+//         })
+
+//       const horario_funcionamento = doc.ref.collection('horario_funcionamento');
+
+//       horario_funcionamento.get()
+//         .then(horaDados => {
+//           horaDados.forEach(horaDoc => {
+//             const dadoshora = horaDoc.data();
+//             console.log("Dados da subcoleção 'Mapa':", dadoshora);
+//           });
+//         })
+
+//     });
+//   })
+//   .catch(error => {
+//     console.error("Erro ao obter dados da coleção 'filiais':", error);
 //   });
 
-// })
+app.get('/Api/dados', (req, res) => {
+  const token_formulario = req.query.token;
 
-const filiaisCollection = db.collection('filiais')
+  userCollection.get()
+    .then(snapshot => {
+      let tokenValido = false;
+      const promises = [];
 
-filiaisCollection.get()
-.then(dados =>{
-   dados.forEach(doc=>{
-      console.log(doc)
-   })
-})
+      snapshot.forEach(doc => {
+        const dados = doc.data();
+        const token_conta = dados.userId;
+
+        if (token_formulario === token_conta) {
+          tokenValido = true;
+
+          const filiaisData = [];
+          const mapaPromises = [];
+          const informacoesContatoPromises = [];
+          const imagensPromises = [];
+          const horarioFuncionamentoPromises = [];
+
+          filiaisCollection.get()
+            .then(filiaisSnapshot => {
+              filiaisSnapshot.forEach(filiaisDoc => {
+                const dadosFiliais = filiaisDoc.data();
+
+                const mapaCollection = filiaisDoc.ref.collection('mapa');
+                const mapaPromise = mapaCollection.get()
+                  .then(mapaSnapshot => {
+                    const Mapa = [];
+                    mapaSnapshot.forEach(mapaDoc => {
+                      const dadosMapa = mapaDoc.data();
+                      Mapa.push(dadosMapa);
+                    });
+                    return Mapa;
+                  })
+                  .catch(error => {
+                    console.error("Erro ao obter dados da subcoleção 'mapa':", error);
+                    return [];
+                  });
+
+                const informacoesContatoCollection = filiaisDoc.ref.collection('informacoes_contato');
+                const informacoesContatoPromise = informacoesContatoCollection.get()
+                  .then(contatosSnapshot => {
+                    const informacoesContato = [];
+                    contatosSnapshot.forEach(contatoDoc => {
+                      const dadoscontatos = contatoDoc.data();
+                      informacoesContato.push(dadoscontatos);
+                    });
+                    return informacoesContato;
+                  })
+                  .catch(error => {
+                    console.error("Erro ao obter dados da subcoleção 'informacoes_contato':", error);
+                    return [];
+                  });
+
+                const imagensCollection = filiaisDoc.ref.collection('imagens');
+                const imagensPromise = imagensCollection.get()
+                  .then(imgSnapshot => {
+                    const imagens = [];
+                    imgSnapshot.forEach(imgDoc => {
+                      const dadosimg = imgDoc.data();
+                      imagens.push(dadosimg);
+                    });
+                    return imagens;
+                  })
+                  .catch(error => {
+                    console.error("Erro ao obter dados da subcoleção 'imagens':", error);
+                    return [];
+                  });
+
+                const horarioFuncionamentoCollection = filiaisDoc.ref.collection('horario_funcionamento');
+                const horarioFuncionamentoPromise = horarioFuncionamentoCollection.get()
+                  .then(horaSnapshot => {
+                    const horarioFuncionamento = [];
+                    horaSnapshot.forEach(horaDoc => {
+                      const dadoshora = horaDoc.data();
+                      horarioFuncionamento.push(dadoshora);
+                    });
+                    return horarioFuncionamento;
+                  })
+                  .catch(error => {
+                    console.error("Erro ao obter dados da subcoleção 'horario_funcionamento':", error);
+                    return [];
+                  });
+
+                mapaPromises.push(mapaPromise);
+                informacoesContatoPromises.push(informacoesContatoPromise);
+                imagensPromises.push(imagensPromise);
+                horarioFuncionamentoPromises.push(horarioFuncionamentoPromise);
+
+                const filialData = {
+                  dadosFiliais: dadosFiliais,
+                  status: 'success',
+                  message: 'Dados recuperados com sucesso',
+                };
+                filiaisData.push(filialData);
+              });
+
+              // Espera todas as promessas serem resolvidas antes de enviar a resposta
+              Promise.all([
+                Promise.all(mapaPromises),
+                Promise.all(informacoesContatoPromises),
+                Promise.all(imagensPromises),
+                Promise.all(horarioFuncionamentoPromises)
+              ])
+                .then(([mapaData, informacoesContatoData, imagensData, horarioFuncionamentoData]) => {
+                  filiaisData.forEach((filialData, index) => {
+                    filialData.mapa = mapaData[index];
+                    filialData.informacoes_contato = informacoesContatoData[index];
+                    filialData.imagens = imagensData[index];
+                    filialData.horario_funcionamento = horarioFuncionamentoData[index];
+                  });
+                  const formattedJSON = JSON.stringify(filiaisData, null, 2);
+                  res.setHeader('Content-Type', 'application/json');
+                  res.send(formattedJSON);
+                })
+                .catch(error => {
+                  console.error("Erro ao obter dados de todas as subcoleções:", error);
+                  res.status(500).json({ error: 'Erro ao obter dados' });
+                });
+            })
+            .catch(error => {
+              console.error("Erro ao obter dados da coleção 'filiais':", error);
+              res.status(500).json({ error: 'Erro ao obter dados' });
+            });
+
+          promises.push(mapaPromises);
+          promises.push(informacoesContatoPromises);
+          promises.push(imagensPromises);
+          promises.push(horarioFuncionamentoPromises);
+        }
+      });
+
+      if (!tokenValido) {
+        res.sendFile(path.join(__dirname, '/../Public', 'Erro405.html'));
+      }
+    })
+    .catch(error => {
+      console.error('Erro ao obter documentos da coleção:', error);
+      res.send("Erro");
+    });
+});
 
 // Configuração do Body Parser para processar dados do formulário
 app.use(bodyParser.urlencoded({ extended: false }));
